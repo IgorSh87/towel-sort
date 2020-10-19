6820306b296c2f435267cfa3459e31d15b31a2e8
igorsh87 <twsaint@rambler.ru>
2020-10-19 13:42:59 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-
-// You should implement your task here.
-
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+  if ((Array.isArray(matrix)) == false) { return []; }
+  else if (matrix.length == 0) { return []; }
+  else {
+      if (matrix.length == 1) {
+          return matrix[0];
+      }
+      else if (matrix.length == 2) {
+          return matrix[0].concat(matrix[1].reverse());
+      }
+      else if (matrix.length == 3) {
+          return matrix[0].concat(matrix[1].reverse()).concat(matrix[2]);
+      }
+      else if (matrix.length == 4) {
+          return matrix[0].concat(matrix[1].reverse()).concat(matrix[2]).concat(matrix[3].reverse());
+      }
+  }
 }
